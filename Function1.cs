37cd9a1d109639dd5d5c46efda4024157fc04041
Girish <girish.nandgawe@gmail.com>
2023-03-27 23:42:15 +0530

    Saved work

--- Function1.cs
+++ Function1.cs
@@ -16,7 +16,9 @@ namespace AppFn
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
+
         {
+            log.LogInformation("Hello World!");
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
