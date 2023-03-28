3d3f7594c57da8ab3efcc32cf7274fdbcef0d49b
Girish <girish.nandgawe@gmail.com>
2023-03-28 10:57:54 +0530

    Saved work

--- Function1.cs
+++ Function1.cs
@@ -14,7 +14,7 @@ namespace AppFn
     {
         [FunctionName("Function1")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
 
         {
